d4e647516bf60a216703e4d97b953cf14ac47477
Thomas Sibley <tsibley@cpan.org>
2013-02-25 23:59:05 +0000

    Import of TSIBLEY/RT-Authen-PAUSE-0.11 from CPAN.
    gitpan-cpan-distribution: RT-Authen-PAUSE
    gitpan-cpan-version:      0.11
    gitpan-cpan-path:         TSIBLEY/RT-Authen-PAUSE-0.11.tar.gz
    gitpan-cpan-author:       TSIBLEY
    gitpan-cpan-maturity:     released

--- lib/RT/Authen/PAUSE.pm
+++ lib/RT/Authen/PAUSE.pm
@@ -4,30 +4,25 @@ RT::Authen::PAUSE - authenticate RT users against PAUSE
 
 =head1 DESCRIPTION
 
-RT's extension that allows people to login into RT using their
-CPANID and passwords. This extension doesn't import user entries
-from CPAN or other perl related sources, so you have to do it
-yourself.
+This RT extension allows people to login into RT using their CPAN ID and
+passwords by proxing requests to PAUSE.  This extension doesn't import user
+entries from CPAN or other Perl related sources, so you have to do it yourself.
 
-To avoid constant requests to the PAUSE service we update user's
-password in RT's DB on successful login, so next time we can avoid
-this step.
-
-People still can change thier passwords in RT, only if they have enough
-rights to do that. That will work and they will be able to use new
-passwords in RT, however we don't push changes back to PAUSE.
+People still can change their passwords in RT, if they have the ModifySelf
+right.  They will be able to use the new password in RT, however we don't push
+changes back to PAUSE.
 
 =cut
 
 package RT::Authen::PAUSE;
 
-our $VERSION = '0.02';
+our $VERSION = '0.11';
 
 1;
 
 =head1 COPYRIGHT
 
-This extension is Copyright (C) 2005 Best Practical Solutions, LLC.
+This extension is Copyright (C) 2005-2013 Best Practical Solutions, LLC.
 
 It is freely redistributable under the terms of version 2 of the GNU GPL.
 
